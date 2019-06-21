d061aaa8fda36adeddc7848b04115ed7d01dfd9f
BastianBueeler <bastian.bueeler@gibmit.ch>
2019-06-21 13:56:24 +0200

    Maven hinzugefügt

---
+++ m154-crm-business/src/main/java/m154_crm/m154_crm_business/App.java
@@ -0,0 +1,13 @@
+package m154_crm.m154_crm_business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
